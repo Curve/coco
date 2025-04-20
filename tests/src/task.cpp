fe5ce9425d257cc93d00474b6385a772f8f555b9
Curve <fynnbwdt@gmail.com>
2025-04-20 02:27:46 +0200

    refactor(tests/task): check `idle`

--- tests/src/task.cpp
+++ tests/src/task.cpp
@@ -9,46 +9,85 @@ using namespace coco::tests;
 // NOLINTNEXTLINE
 suite<"task"> task_test = []
 {
-    static auto compute = []() -> coco::task<int>
+    "eager"_test = []
     {
-        co_await co_sleep{std::chrono::milliseconds{500}};
-        co_return 10;
+        static auto compute = []() -> coco::task<int>
+        {
+            co_await co_sleep{std::chrono::milliseconds{500}};
+            co_return 10;
+        };
+
+        static auto await = [](std::promise<int> promise) -> coco::basic_task
+        {
+            auto result = co_await compute();
+            promise.set_value(result + 10);
+        };
+
+        "eager"_test = []
+        {
+            auto task = compute();
+
+            const auto &future = static_cast<const std::future<int> &>(task);
+            auto status        = future.wait_for(std::chrono::seconds(1));
+
+            expect(status == std::future_status::ready);
+            expect(eq(task.get(), 10));
+        };
+
+        "get"_test = []
+        {
+            expect(eq(compute().get(), 10));
+        };
+
+        "co_await"_test = []
+        {
+            auto promise = std::promise<int>{};
+            auto future  = promise.get_future();
+
+            await(std::move(promise)).get();
+
+            expect(future.wait_for(std::chrono::seconds(0)) == std::future_status::ready);
+            expect(future.get() == 20);
+        };
+
+        "then"_test = []
+        {
+            auto promise = std::promise<int>{};
+            auto future  = promise.get_future();
+
+            compute().then(
+                [promise = std::move(promise)](auto result) mutable
+                {
+                    expect(eq(result, 10));
+                    promise.set_value(result);
+                });
+
+            expect(eq(future.get(), 10));
+        };
     };
 
-    static auto await = [](std::promise<int> promise) -> coco::basic_task
+    "lazy"_test = []
     {
-        auto result = co_await compute();
-        promise.set_value(result + 10);
-    };
+        static auto compute = []() -> coco::task<int>
+        {
+            co_await coco::task<int>::idle{};
+            co_return 10;
+        };
 
-    "get"_test = []
-    {
-        expect(eq(compute().get(), 10));
-    };
+        static auto await = [](coco::task<int> &awaitable) -> coco::task<int>
+        {
+            co_return co_await std::move(awaitable);
+        };
 
-    "co_await"_test = []
-    {
-        auto promise = std::promise<int>{};
-        auto future  = promise.get_future();
+        "not-eager"_test = []
+        {
+            auto task = compute();
 
-        await(std::move(promise)).get();
+            const auto &future = static_cast<const std::future<int> &>(task);
+            auto status        = future.wait_for(std::chrono::seconds(1));
 
-        expect(future.wait_for(std::chrono::seconds(0)) == std::future_status::ready);
-        expect(future.get() == 20);
-    };
-
-    "then"_test = []
-    {
-        auto promise = std::promise<int>{};
-        auto future  = promise.get_future();
-
-        compute().then(
-            [promise = std::move(promise)](auto result) mutable
-            {
-                expect(eq(result, 10));
-                promise.set_value(result);
-            });
-
-        expect(eq(future.get(), 10));
+            expect(status != std::future_status::ready);
+            expect(eq(await(task).get(), 10));
+        };
     };
 };
